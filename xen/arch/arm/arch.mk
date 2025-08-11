1915b1f1dfbe2f7cc576e1820b959ad5fdd73f24
Erkai Yu <si1krow@outlook.com>
2025-08-11 00:08:20 -0500

    disabled a fp related option, otherwise compilation error by clang for ARM64

--- xen/arch/arm/arch.mk
+++ xen/arch/arm/arch.mk
@@ -14,7 +14,7 @@ CFLAGS-$(CONFIG_ARM_64) += -march=armv8-r
 else
 CFLAGS-$(CONFIG_ARM_64) += -mcpu=generic
 endif
-CFLAGS-$(CONFIG_ARM_64) += -mgeneral-regs-only # No fp registers etc
+# CFLAGS-$(CONFIG_ARM_64) += -mgeneral-regs-only # No fp registers etc
 $(call cc-option-add,CFLAGS-$(CONFIG_ARM_64),CC,-mno-outline-atomics)
 
 ifneq ($(filter command line environment,$(origin CONFIG_EARLY_PRINTK)),)
